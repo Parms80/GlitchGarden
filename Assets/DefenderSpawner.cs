8e1403f5b284b68eb2289fa355d3f3eb95bfb6b1
Perminder Suman <permindersuman@Perminders-MBP.lan>
2017-12-27 10:35:26 +0000

    Got selector bar working. Clicking on a grid space places selected defender there

---
+++ Assets/DefenderSpawner.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DefenderSpawner : MonoBehaviour {
+
+	public Camera camera;
+	private GameObject parent;
+
+	// Use this for initialization
+	void Start () {
+
+		parent = GameObject.Find ("Defenders");
+		if (!parent) {
+			parent = new GameObject ("Projectiles");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown() {
+		AddDefenderToGrid ();
+	}
+
+	void AddDefenderToGrid() {
+		Vector2 rawPos = CalculateWorldPointOfMouseClick ();
+		Vector2 roundedPos = SnapToGrid (rawPos);
+		GameObject defender = Button.selectedDefender;
+		Quaternion zeroRot = Quaternion.identity;
+		GameObject newDefender = Instantiate (defender, roundedPos, zeroRot) as GameObject;
+		newDefender.transform.parent = parent.transform;
+	}
+
+	Vector2 CalculateWorldPointOfMouseClick() {
+		Vector3 worldPoint = camera.ScreenToWorldPoint (Input.mousePosition);
+		return new Vector2 (worldPoint.x, worldPoint.y);
+	}
+
+	Vector2 SnapToGrid (Vector2 rawWorldPos) {
+		float newX = Mathf.RoundToInt (rawWorldPos.x);
+		float newY = Mathf.RoundToInt (rawWorldPos.y);
+		return new Vector2 (newX, newY);
+	}
+}
